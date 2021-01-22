0303464cecaabd71c0b5e31f7e22cce661291c74
QAEJasonTompkins <55994191+QAEJasonTompkins@users.noreply.github.com>
2021-01-21 20:39:04 -0500

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,23 @@
+package com.practice.testcases;
+
+public class ForLoops {
+	
+	public static void main(String[] args) {
+		
+		for(int i =0; i < 10; i++) {
+			System.out.println("I Love Animals!");
+		}
+		
+		for(int i = 0; i < 15; i++) {
+			System.out.println(i);
+			
+		}
+		
+		int[] grades = {98, 100, 75, 90, 99};
+		
+		for(int i = 0; i < grades.length; i++) {
+			System.out.println(grades[i]);
+		}
+	}
+
+}
