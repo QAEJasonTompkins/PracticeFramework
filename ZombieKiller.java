2954a70f57cd48bbd19a1ee55689acaf1bc00225
QAEJasonTompkins <55994191+QAEJasonTompkins@users.noreply.github.com>
2021-01-22 11:28:07 -0500

    Add files via upload

---
+++ ZombieKiller.java
@@ -0,0 +1,27 @@
+package com.practice.testcases;
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our zombie kill program!!!");
+		
+		String backpack[] = {"Shotgun", "Assault Rifle", "Sniper", "Pistol"};
+		String zombies[] = {"Slow Walker", "Heavy", "Tank", "Banshee", "Hoarde"};
+		int numbersZombiesHATE[] = {34, 21, 78, 200, 600, 666};
+		
+		System.out.println("Backpack Weapons!!!");
+		System.out.println(backpack [0]);
+		System.out.println(backpack [1]);
+		System.out.println(backpack [2]);
+		System.out.println(backpack [3]);
+		
+		System.out.println("Heavy is attacking!!!");
+		System.out.println(zombies[1]);
+		System.out.println("Choose the right weapon!");
+		System.out.println(backpack[0]);
+		System.out.println("Good choice! Zombie Down!");
+
+
+	}
+
+}
