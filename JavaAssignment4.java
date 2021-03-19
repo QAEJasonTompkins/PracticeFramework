ecde8050206192237e3728fe521e4cc8bb90cffe
QAEJasonTompkins <55994191+QAEJasonTompkins@users.noreply.github.com>
2021-03-19 14:43:09 -0400

    Add files via upload

--- JavaAssignment4.java
+++ JavaAssignment4.java
@@ -1,19 +1,16 @@
 package com.practice.assignments;
 
+//create a function that takes two integers as parameters and returns the product
 public class JavaAssignment4 {
 	
-	static int myFunc() {
-		
-		int x = 4;
-		int y = 8;
-				
+	static int myFunc(int x, int y) {		
 		return x + y;
 	}
 
 	public static void main(String[] args) {
 
 	//Call your method myFunc() and print the product of 4 and 8
-		System.out.println(myFunc());
+		System.out.println("The product of x and y is " + myFunc(4, 8));
 
 	}
 
