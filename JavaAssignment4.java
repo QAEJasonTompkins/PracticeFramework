8c02bef774a80eacc65e725c79de093a3fcbfda3
QAEJasonTompkins <55994191+QAEJasonTompkins@users.noreply.github.com>
2021-03-21 16:23:36 -0400

    Add files via upload

--- JavaAssignment4.java
+++ JavaAssignment4.java
@@ -4,13 +4,15 @@ package com.practice.assignments;
 public class JavaAssignment4 {
 	
 	static int myFunc(int x, int y) {		
-		return x + y;
+		return x * y;
 	}
 
 	public static void main(String[] args) {
 
 	//Call your method myFunc() and print the product of 4 and 8
 		System.out.println("The product of x and y is " + myFunc(4, 8));
+		System.out.println("The product of x and y is " + myFunc(6, 9));
+
 
 	}
 
